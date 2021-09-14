fab98c8443f0e5b4f55136a3099b8dc99ce04cb8
Alec <drosualec@gmail.com>
2021-09-14 00:19:32 -0400

    Add Card Deletion

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@
 const editProfileButton = document.querySelector('.title__button');
 const infoTitle = document.querySelector('.title__name');
 const infoSubtitle = document.querySelector('.info__job');
+const previewModal = document.querySelector('.modal_type_preview');
 
 const editModal = document.querySelector('.modal_type_edit');
 const addModal = document.querySelector('.modal_type_add');
@@ -23,19 +24,12 @@ const addImage = addForm.querySelector('.form__input_type_image-url');
 
 const addCard = document.querySelector('.profile__button');
 const addModalCloseBtn = addModal.querySelector('.modal__close-btn');
+const previewModalCloseBtn = previewModal.querySelector('.modal__close-btn');
+const previewModalImg = previewModal.querySelector('.modal__img');
+const previewModalCaption = previewModal.querySelector('.modal__caption');
 
+const fadeModal = document.querySelectorAll('.modal');
 // Functions
-
-function generateCards(card) {
-    // use template
-    const cardEl = elementTemplate.cloneNode(true);
-    // set title
-    cardEl.querySelector('.text__label').textContent = card.title;
-    // set image
-    cardEl.querySelector('.element__img').src = card.image;
-
-    return cardEl;
-}
 
 function fillEditForm(editModal) {
         if (!editModal.classList.contains('modal_is-open')) {
@@ -56,6 +50,39 @@ function saveProfile(event) {
     toggleForm(editModal);
 }
 
+function showPreview(card) {
+    previewModalImg.src = card.image;
+    previewModalCaption.textContent = card.title;
+    toggleForm(previewModal);
+}
+
+function generateCards(card) {
+    // use template
+    const cardEl = elementTemplate.cloneNode(true);
+
+    const cardImage = cardEl.querySelector('.element__img');
+    const trash = cardEl.querySelector('.element__trash');
+
+    // set title
+    cardEl.querySelector('.text__label').textContent = card.title;
+    // set image
+    cardEl.querySelector('.element__img').src = card.image;
+    // Attach Events
+    cardImage.addEventListener('click', () => showPreview(card));
+
+    cardEl.addEventListener('click', function (evt) {
+        evt.target.classList.toggle('text__heart_active');
+    });
+
+    
+    trash.addEventListener('click', function() {
+        cardEl.parentNode.removeChild(cardEl);
+    });
+
+    return cardEl;
+
+}
+
 function createCard(event) {
     event.preventDefault(addForm);
     const card = {
@@ -64,25 +91,32 @@ function createCard(event) {
     };
     const cardEl = generateCards(card);
     elements.prepend(cardEl);
+
     toggleForm(addModal);
 }
 
 // Event Listeners
 
+
+
+
 editForm.addEventListener('submit', saveProfile);
 editProfileButton.addEventListener('click', () => {
     fillEditForm(editModal);
-    toggleForm(editModal)
+    toggleForm(editModal);
 });
 editModalCloseBtn.addEventListener('click', () => toggleForm(editModal));
 addForm.addEventListener('submit', createCard);
 addCard.addEventListener('click', () => toggleForm(addModal));
 addModalCloseBtn.addEventListener('click', () => toggleForm(addModal));
+previewModalCloseBtn.addEventListener('click', () => toggleForm(previewModal));
+
+
 
 // Actions
 
 initialCards.forEach(card => {
     cardEl = generateCards(card);
-    // append to list
+
     elements.append(cardEl);
-})
+});
